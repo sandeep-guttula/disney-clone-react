122f3c27aa6e49e078372c079e45bfff81a1f823
sandeep-0701 <sandeepguttula2002@gmail.com>
2022-09-06 23:33:22 +0530

    Nav Compeleted

---
+++ src/components/ImageSlider.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function ImageSlider() {
+  return (
+    <div>ImageSlider</div>
+  )
+}
+
+export default ImageSlider
